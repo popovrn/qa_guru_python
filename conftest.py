aff6408b0daf909a1ee9f05bd727c279efa103a3
popov_r_n <popov_r_n@magnit.ru>
2023-09-22 13:33:34 +0300

    Исправлены ошибки вывода, объеденены фикстуры

--- conftest.py
+++ conftest.py
@@ -1,18 +1,13 @@
 import pytest
+from selene import  browser
 from selene.support.shared import browser
 
 
-@pytest.fixture(scope='session')
-def browser_size():
+@pytest.fixture()
+def browser_setings():
     browser.config.window_width = 1024
     browser.config.window_height = 768
-
-
-@pytest.fixture(scope='function')
-def browser_open(browser_size):
     browser.open('https://google.com')
-    yield
-    browser.quit()
 
 
 
--- test_dz2.py
+++ test_dz2.py
@@ -1,12 +1,13 @@
 from selene import  be, have, browser
 
-def test_first_less1(browser_open):
+def test_first_less1(browser_setings):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
 
-def test_second_less2(browser_open):
-    browser.element('[name="q"]').should(be.blank).type('abrakadabranaoborot').press_enter()
-    browser.element('[id="search"]').should(have.no.text('abrakadabranaoborot'))
+def test_second_less2(browser_setings):
+    browser.element('[name="q"]').should(be.blank).type('988897hfgfytmmjmg76jt76jgkjhjghgf8687tmg86u7gjgf78775tjmgjk').press_enter()
+    browser.element('[id="result-stats""]').should(have.text('988897hfgfytmmjmg76jt76jgkjhjghgf8687tmg86u7gjgf78775tjmgjk'))
+
 
 
 
